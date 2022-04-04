1763b0b676c3ce2c05139fe0ddcd1048c8af5215
Max Castro Vidal <maxcastrovidal@gmail.com>
2022-04-04 19:37:43 -0400

    Agregar archivos de proyecto.

---
+++ Controllers/UsuariosController.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Newtonsoft.Json;
+
+
+namespace CoreApp.Controllers
+{
+    public class Usuarios : Controller
+    {
+        private Procs procs;
+
+        public Usuarios()
+        {
+            procs = new Procs();
+        }
+
+        public string Index()
+        {
+            return "Hola Mundo";
+        }
+
+        [HttpGet]
+        public JsonResult Info( int? id,
+                                string? Nombre,
+	                            int? EdadDesde,
+	                            int? EdadHasta,
+	                            int? Activo,
+                                string? FecCreacionDesde,
+                                string? FecCreacionHasta,
+	                            string? Ordenar)
+        {
+
+            string vSQL = "EXEC UsuariosInfo ";
+            if (id != null) {vSQL += "@Id=" + id.ToString() + ", ";}
+            if (Nombre != null) { vSQL += "@Nombre='" + Nombre + "', "; }
+            if (EdadDesde != null) { vSQL += "@EdadDesde=" + EdadDesde.ToString() + ", "; }
+            if (EdadHasta != null) { vSQL += "@EdadHasta=" + EdadHasta.ToString() + ", "; }
+            if (Activo != null) { vSQL += "@Activo=" + id.ToString() + ", "; }
+            if (FecCreacionDesde != null) { vSQL += "@FecCreacionDesde='" + FecCreacionDesde + "', "; }
+            if (FecCreacionHasta != null) { vSQL += "@FecCreacionHasta='" + FecCreacionHasta + "', "; }
+            if (Ordenar != null) { vSQL += "@Ordenar='" + Ordenar + "', "; }
+
+            if (vSQL != "EXEC UsuariosInfo ")
+            {
+                vSQL = vSQL.Substring(0, vSQL.Length - 2);
+            }
+
+            SqlDataReader dr = procs.EjecutarSQL(vSQL);
+
+            var datatable = new System.Data.DataTable();
+            datatable.Load(dr);
+            string JsonResponse = string.Empty;
+            JsonResponse = JsonConvert.SerializeObject(datatable);
+
+            System.Text.Json.JsonSerializerOptions jsonSerializerOptions = new System.Text.Json.JsonSerializerOptions();
+            jsonSerializerOptions.WriteIndented = true;
+
+            return new JsonResult(JsonResponse, jsonSerializerOptions);
+
+        }
+
+        public JsonResult Gest(int? id,
+                                string? Nombre,
+                                int? Edad,
+                                string? Activo)
+        {
+
+            string vSQL = "EXEC UsuariosGest ";
+            if (id != null) { vSQL += "@Id=" + id.ToString() + ", "; }
+            if (Nombre != null) { vSQL += "@Nombre='" + Nombre + "', "; }
+            if (Edad != null) { vSQL += "@Edad=" + Edad.ToString() + ", "; }
+
+            if (Activo=="True" || Activo=="true" || Activo == "1")
+            {
+                vSQL += "@Activo=1, ";
+            } else
+            {
+                vSQL += "@Activo=0, ";
+            }
+
+             
+
+            if (vSQL != "EXEC UsuariosGest ")
+            {
+                vSQL = vSQL.Substring(0, vSQL.Length - 2);
+            }
+
+            SqlDataReader dr = procs.EjecutarSQL(vSQL);
+
+            var datatable = new System.Data.DataTable();
+            datatable.Load(dr);
+            string JsonResponse = string.Empty;
+            JsonResponse = JsonConvert.SerializeObject(datatable);
+
+            System.Text.Json.JsonSerializerOptions jsonSerializerOptions = new System.Text.Json.JsonSerializerOptions();
+            jsonSerializerOptions.WriteIndented = true;
+
+            return new JsonResult(JsonResponse, jsonSerializerOptions);
+
+        }
+
+    }
+}
---
+++ Procs.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace CoreApp
+{
+
+    public class Procs
+    {
+        SqlConnection cnn;
+
+        public Procs()
+        {
+            //string vcadenaConexion = "Server=(localdb)\\MSSQLLocalDB;Database=MXTest;Trusted_Connection=True;MultipleActiveResultSets=true";
+            string vcadenaConexion = "Data Source=SQL5080.site4now.net;Initial Catalog=db_a8331e_test001;User Id=db_a8331e_test001_admin;Password=1122QQww..sa;TrustServerCertificate=True";
+            cnn = new SqlConnection(vcadenaConexion);            
+            cnn.Open();
+        }
+
+        public SqlDataReader EjecutarSQL(string vSQL)
+        {
+            SqlCommand cmd = new SqlCommand(vSQL, cnn);
+            cmd.CommandType = System.Data.CommandType.Text;
+
+            SqlDataReader dr = cmd.ExecuteReader();
+            return dr;
+        }
+
+
+
+    }
+}
